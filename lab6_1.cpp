acdec464a594d17716173028dadf9be99b9830bb
Korn Sriwattana <Korn_s@cmu.ac.th>
2023-12-01 14:37:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    
+    int i =1;
+    int even = 0;
+    int odd = 0;
+    
+    while( i != 0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        if(i !=0 ){
+            if( i%2 == 0){
+            even++;
+            }else{
+            odd++;
+            }
+    }
+}
+    cout << "#Even numbers = " << even;
+    cout << "\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
